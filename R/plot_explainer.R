eb347208ef19ab4c8aab78a33aa18724a3db50b0
Alicja Gosiewska <alicjagosiewska@gmail.com>
2018-07-06 23:08:00 +0200

    Add survminer to plot.surv_explainer

--- R/plot_explainer.R
+++ R/plot_explainer.R
@@ -1,29 +1,21 @@
 #' @title Plot for surv_explainer object
-#' 
-#' @description Function plot for surv_explainer object visualise estimated survival curve of mean probabilities in chosen time points.  
+#'
+#' @description Function plot for surv_explainer object visualise estimated survival curve of mean probabilities in chosen time points.
 #'
 #' @param x object of class "surv_explainer"
 #' @param times numeric vector of time points
-#' @param ... other arguments
+#' @param ... other arguments for function \code{\link[survminer]{ggsurvplot}}
 #'
 #' @import ggplot2
-#' 
+#' @importFrom survival survfit
+#' @importFrom survminer ggsurvplot
+#'
 #' @method plot surv_explainer
-#' @export 
+#' @export
 
 
-plot.surv_explainer <- function(x, times=1, ...){
-  time <- prob <- NULL
-   new_pred <- function(model, data){
-    x$predict_function(model, data, times)
-  }
-  
-  times <- seq(min(x$time), max(x$time), by=1)
-  times <- floor(times)
-  predicted_values <- new_pred(x$model, x$data)
-  predicted_values <- colMeans(predicted_values, na.rm=T)
-  df <- data.frame(time = times, prob = predicted_values)
-  plot_explainer <- ggplot(df, aes(time, prob)) + geom_line() + theme_bw()
-  plot_explainer
-  
-} 
+plot.surv_explainer <- function(x, ...){
+  fit <- survfit(x$model, data = x$data)
+  ggsurvplot(fit, ...)
+
+}
