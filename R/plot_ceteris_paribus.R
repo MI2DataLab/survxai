bff039d91778b0e71e6e5721c50e1d8a93810a77
Alicja Gosiewska <alicjagosiewska@gmail.com>
2018-07-17 12:11:41 +0200

    Fix y axis ceteris paribus

--- R/plot_ceteris_paribus.R
+++ R/plot_ceteris_paribus.R
@@ -7,19 +7,19 @@
 #' @param ... other arguments
 #'
 #' @import ggplot2
-#' @examples 
+#' @examples
 #' \dontrun{
 #' library(survxai)
-#' library(rms) 
+#' library(rms)
 #' library(randomForestSRC)
 #' data(pbc, package = "randomForestSRC")
 #' pbc <- pbc[complete.cases(pbc),]
-#' predict_times <- function(model, data, times){ 
+#' predict_times <- function(model, data, times){
 #'                   prob <- rms::survest(model, data, times = times)$surv
 #'                   return(prob)
 #'                   }
 #' cph_model <- cph(Surv(days/365, status)~., data=pbc, surv=TRUE, x = TRUE, y=TRUE)
-#' surve_cph <- explain(model = cph_model, data = pbc[,-c(1,2)], y = Surv(pbc$days/365, pbc$status), 
+#' surve_cph <- explain(model = cph_model, data = pbc[,-c(1,2)], y = Surv(pbc$days/365, pbc$status),
 #'              predict_function = predict_times)
 #' cp_cph <- ceteris_paribus(surve_cph, pbc[1,-c(1,2)])
 #' plot(cp_cph)
@@ -30,33 +30,33 @@
 plot.surv_ceteris_paribus_explainer <- function(x, selected_variable = NULL, ...) {
   y_hat <- new_x <- time <- NULL
   dfl <- c(list(x), list(...))
-  
+
   all_responses <- do.call(rbind, dfl)
   class(all_responses) <- "data.frame"
-  
-  
+
+
   if(!is.null(selected_variable) && !(selected_variable %in% factor(all_responses$vname))){
     stop(paste0("Selected variable ", selected_variable, "not present in surv_ceteris_paribus object."))
   }
-  
+
   all_predictions <- lapply(dfl, function(tmp) {
     pred <- attr(tmp, "prediction")
     data.frame(prediction = pred$new_y_hat,
                label = tmp$label[1])
   })
-  
+
   times <- lapply(dfl, function(tmp) {
     pred <- attr(tmp, "prediction")
     data.frame(prediction = pred$times)
   })
-  
+
   times <- do.call(rbind, times)
   all_predictions <- do.call(rbind, all_predictions)
   all_predictions <- data.frame(t(all_predictions[,-ncol(all_predictions)]))
-  
+
   all_predictions$time_2 <- times$prediction
   colnames(all_predictions)[1] <- "y_hat_2"
-  
+
   if(!is.null(selected_variable)){
     all_responses <- all_responses[which(all_responses$vname == selected_variable),]
     legend <- unique(all_responses$vname)
@@ -66,11 +66,14 @@ plot.surv_ceteris_paribus_explainer <- function(x, selected_variable = NULL, ...
     add_theme <- theme(legend.position = "none")
     facet <- facet_wrap(~vname)
   }
-  
+
   pl <- ggplot(all_responses, aes(x = time, y = y_hat, col = factor(new_x)))+
     geom_step()+
-    geom_step(data = all_predictions, aes(x = time_2, y = y_hat_2), col="black")
-  
+    geom_step(data = all_predictions, aes(x = time_2, y = y_hat_2), col="black") +
+    scale_y_continuous(breaks = seq(0,1,0.1),
+                       labels = paste(seq(0,100,10),"%"),
+                       name = "survival probability")
+
   pl + facet +
     theme_mi2()+
     add_theme
