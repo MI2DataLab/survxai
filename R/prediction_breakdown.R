cce6badcf483a626ec7d8fab7e4697190a239bac
Alicja Gosiewska <alicjagosiewska@gmail.com>
2018-07-25 14:40:59 +0200

    add time and prob parameters to prediction_breakdown function
    #5

--- R/prediction_breakdown.R
+++ R/prediction_breakdown.R
@@ -4,6 +4,8 @@
 #'
 #' @param explainer an object of the class 'surv_explainer'
 #' @param observation a new observation to explain
+#' @param time time point at which variable contributions are computed. If NULL median time is taken.
+#' @param prob survival probability at which variable contributions are computed
 #' @param ... other parameters
 #'
 #' @return An object of class surv_prediction_breakdown_explainer
@@ -30,24 +32,53 @@
 #' @export
 
 
-prediction_breakdown <- function(explainer, observation, ...){
-
+prediction_breakdown <- function(explainer, observation, time = NULL, prob = NULL, ...){
   if (!("surv_explainer" %in% class(explainer))) stop("The prediction_breakdown() function requires an object created with explain() function form survxai package.")
   if (is.null(explainer$data)) stop("The prediction_breakdown() function requires explainers created with specified 'data' parameter.")
+  if (!is.null(time) & !is.null(prob)) stop("Only one of the parameters 'time', 'prob' should be provided.")
 
   # breakDown
+  if (is.null(prob)) {
+    if (is.null(time)) time <- median(explainer$times)
 
-  time <- median(explainer$times)
-  new_pred <- function(model, data){
+    new_pred <- function(model, data){
       explainer$predict_function(model, data, times = time)
+    }
+  } else {
+    times_sorted <- sort(explainer$times)
+
+    find_time <- function(x){
+      tim <- (x < prob)
+      index <- c(min(which(tim == TRUE)) -1, min(which(tim == TRUE)))
+      closest_times <- times_sorted[index]
+      weighted.mean(closest_times, x[index])
+    }
+
+    new_pred <- function(model, data){
+      probabilities <- explainer$predict_function(model, data, times = explainer$times)
+      probabilities <- as.data.frame(probabilities)
+
+     res <- apply(probabilities, MARGIN = 1, FUN = find_time)
+     res <- na.omit(res)
+     return(res)
+
+    }
+
+    npred <- new_pred(explainer$model, explainer$data)
+    message("Number of observations with prob > ", prob, ": ", nrow(explainer$data) - length(npred))
   }
 
 
+
+
+  oldw <- getOption("warn")
+  options(warn = -1)
   res<- broken(model = explainer$model,
                   new_observation = observation,
                   data = explainer$data,
                   predict.function = new_pred,
                   baseline = "Intercept")
+  options(warn = oldw)
 
   class(res) <- "data.frame"
 
@@ -98,7 +129,8 @@ prediction_breakdown <- function(explainer, observation, ...){
   res <- res[,c("contribution", "variable_name")]
   colnames(res)[2] <- "variable"
   attr(result, "contribution") <- res
-
+  attr(result, "time") <- time
+  attr(result, "prob") <- prob
   class(result) <- c("surv_prediction_breakdown_explainer", "data.frame")
   result
 
