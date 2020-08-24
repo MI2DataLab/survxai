736deffbd8ebff2aef538ba4abd6184f2e2503df
jusira <alicjagosiewska@gmail.com>
2020-08-24 21:53:04 +0200

    Fix length of lines in the documentation

--- R/model_performance.R
+++ R/model_performance.R
@@ -17,7 +17,8 @@
 #'    library(rms)
 #'    data("pbcTrain")
 #'    data("pbcTest")
-#'    cph_model <- cph(Surv(years, status)~sex + bili + stage, data=pbcTrain, surv=TRUE, x = TRUE, y=TRUE)
+#'    cph_model <- cph(Surv(years, status)~ sex + bili + stage,
+#'      data=pbcTrain, surv=TRUE, x = TRUE, y=TRUE)
 #'    surve_cph <- explain(model = cph_model, data = pbcTest[,-c(1,5)],
 #'                         y = Surv(pbcTest$years, pbcTest$status))
 #'    mp_cph <- model_performance(surve_cph)
